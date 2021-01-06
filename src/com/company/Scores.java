ca1b549a1f5078654db133deb946881fa5ab17e4
AmandineLabaeye <amandine.labaeye@orange.fr>
2021-01-06 13:25:54 +0100

    Modification de la class Scores
    Création des fonctions d'affichage croissant et décroissant, avec des boucles préparation avec commentaires et indications de la fonction modifier les scores.

--- src/com/company/Scores.java
+++ src/com/company/Scores.java
@@ -27,12 +27,26 @@ public class Scores {
 
     public static void menuScore() {
 
+        // Tableau temporaire de la liste des joueurs et des scores correspondant au nom
+        String[][] monTableau = {
+                {"Thiphaine", "50"},
+                {"Isabelle", "46"},
+                {"Amandine", "45"},
+                {"Anthony", "38"},
+                {"Alan", "34"},
+                {"Sandrine", "32"},
+                {"Toinon", "23"},
+                {"Christophe", "21"},
+                {"Killian", "20"},
+                {"Patrick", "12"},
+        };
+
         // Sous menu du score
-        System.out.println(ansi().reset().fg(Ansi.Color.WHITE).a("* Veuillez saisir la lettre de l'option voulu *"));
-        System.out.println(ansi().reset().fg(Ansi.Color.MAGENTA).a("* - C : Voir le score croissant               *"));
-        System.out.println(ansi().reset().fg(Ansi.Color.BLUE).a("* - D : Voir le score décroissant             *"));
-        System.out.println(ansi().reset().fg(Ansi.Color.GREEN).a("* - M : Retour au menu principal              *"));
-        System.out.println(ansi().reset().fg(Ansi.Color.RED).a("* - E : Pour quitter l'application            *"));
+        System.out.println(ansi().reset().fg(Ansi.Color.WHITE).a("* Veuillez saisir la lettre de l'option voulu               *"));
+        System.out.println(ansi().reset().fg(Ansi.Color.BLUE).a("* - D : Voir le score (Du meilleur au moins fort)           *"));
+        System.out.println(ansi().reset().fg(Ansi.Color.MAGENTA).a("* - C : Voir le score croissant (Du moins fort au meilleur) *"));
+        System.out.println(ansi().reset().fg(Ansi.Color.GREEN).a("* - M : Retour au menu principal                            *"));
+        System.out.println(ansi().reset().fg(Ansi.Color.RED).a("* - E : Pour quitter l'application                          *"));
 
 
         //On récupérer et demande à l'utilisateur d'entrée son choix
@@ -46,11 +60,11 @@ public class Scores {
             switch (saisieUtilisateur) {
                 // Si l'option entrée est C alors on lui affiche la liste des scores par ordre croissant
                 case "c", "C" -> {
-                    System.out.println("Affichage du score par ordre croissant");
+                    affichageScoreDecroissant(monTableau);
                 }
                 // Si l'option entrée est D alors on lui affiche la liste des scores par ordre décroissant
                 case "d", "D" -> {
-                    System.out.println("Affichage du score par ordre décroissant");
+                    affichageScoreCroissant(monTableau);
                 }
                 // Si l'option entrée est M alors on lui affiche le menu principal du jeu
                 case "m", "M" -> {
@@ -65,6 +79,43 @@ public class Scores {
             System.out.println(ansi().reset().fg(Ansi.Color.RED).a("Vous devez entrer une des propositions demandées"));
             menuScore();
         }
+
+    }
+
+    public static void affichageScoreDecroissant(String[][] monTableau) {
+
+        // Affichage des scores dans l'odre décroissant (meilleur au moins fort)
+
+        System.out.println("Tableau de scores : (Ordre Décroissant)");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(ansi().reset().fg(Ansi.Color.BLUE).a(monTableau[i][0] + ", " + monTableau[i][1]));
+        }
+
+        menuScore();
+
+    }
+
+    public static void affichageScoreCroissant(String[][] monTableau) {
+
+        // Affichage des scores dans l'ordre croissant (moins fort au meilleur)
+
+        System.out.println("Tableau de scores : (Ordre Croissant)");
+
+        //
+        for (int i = monTableau.length - 1; i >= 0; i--) {
+            System.out.println(ansi().reset().fg(Ansi.Color.BLUE).a(monTableau[i][0] + ", " + monTableau[i][1]));
+        }
+
+        menuScore();
+
+    }
+
+    public static void modifierLesScores() {
+
+        // Faire l'ajout ou la modif dans le tableau et renvoyer le tableau pour retrier à chaque fois dans les fonctions d'affichages
+        // Ajout et Modif (Commencement de partie et de choix du pseudo si déjà existant on actualise le score correspondant donc retrie derrière)
+
     }
 
 
